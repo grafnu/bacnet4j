4ddcf7b6e8d78b76e770c112ae8646bdb014dffb
Trevor Pering <peringknife@google.com>
2017-05-15 00:05:22 -0700

    Remove unecessary cruft, and import some utilities

--- src/main/java/com/serotonin/bacnet4j/npdu/mstp/FrameResponseListener.java
+++
@@ -1,7 +0,0 @@
-package com.serotonin.bacnet4j.npdu.mstp;
-
-public interface FrameResponseListener {
-    void response(Frame frame);
-
-    void timeout();
-}
---
+++ src/main/java/com/serotonin/util/IpWhiteListException.java
@@ -0,0 +1,12 @@
+package com.serotonin.util;
+
+public class IpWhiteListException extends Exception
+{
+
+ public IpWhiteListException(String message)
+ {
+     super(message);
+ }
+
+ private static final long serialVersionUID = -1L;
+}
